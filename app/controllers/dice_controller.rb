a78a439d3a18dc4d13c4952ed124819e653e91c1
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-04-14 21:15:58 +0000

    dice completed

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,24 @@
 class DiceController < ApplicationController
 
+  
+  def infinity_and_beyond   
+      render({:template => "dice_templates/infinity.html.erb"})
+  end
+
+  def infinity_and_beyond
+    @num_dice = params.fetch("rolls")
+    @num_sides = params.fetch("sides")
+
+    @array_of_rolls = Array.new
+
+    @num_dice.to_i.times do
+      @array_of_rolls.push(rand(@num_sides.to_i) + 1)
+    end
+
+    render({ :template => "dice_templates/infinity.html.erb"})
+  end
+
+
   def one_six
     @array_of_rolls = Array.new
 
